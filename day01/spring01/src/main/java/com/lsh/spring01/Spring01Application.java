12a2bbb68f11e6e32cd02af372a48e8c8b74492b
2sseong <sh999051@naver.com>
2025-04-01 16:44:41 +0900

    커밋

--- day01/spring01/src/main/java/com/lsh/spring01/Spring01Application.java
+++ day01/spring01/src/main/java/com/lsh/spring01/Spring01Application.java
@@ -11,24 +11,34 @@ public class Spring01Application {
 
 		System.out.println("Hello, Spring Boot");
 
-		// 변수, 자료현연습
+		// 변수, 자료형연습
 		int account = 10000000;
 		System.out.println("계좌금액은"+ account);
 		float pi = 3.141592f;
 		System.out.println("Pi는"+pi);
-		char ch_first = "A";
+		char ch_first = 'A';
 		System.out.println("문자는" + ch_first);
 
 		// 연산자 연습
 		int a = 17;
-		int b =25;
-		System.out.println("a+b - %d\n");
-		float divresult = b/a;
-		System.out.println("a %% b = %f/n", divresult);
+		int b =24;
+		System.out.printf("a+b = %d\n",a+b);
+		float divresult = b % a;
+		System.out.printf("b %% a = %f\n", divresult);
 		
-		a++;
-		System.out.println("a++=" +a++);
+		
+		System.out.println("a++="+ a++); // 연산자 우선순위
+		System.out.println("++a="+ ++a);
 
+		//비트연산
+		System.out.println("40 & 124 = " + (40&124));
+		// 40 = 0010 1000
+		// 124 = 0111 1100
+		// 0000 0010 <<2 0000 1000
+
+		boolean case1 = 40 >20; //true
+		boolean case2 = 12<=13; //true
+		System.out.println("case1&&case2 = " + (case1 && case2));
 
 	}
 
